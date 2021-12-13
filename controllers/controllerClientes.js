daea9fcad6b2d6ca570d3f34e90734f1f9e96258
Danilo Serafim <danilob.serafim@gmail.com>
2021-12-13 16:26:58 -0300

    desfazendo coisas

--- controllers/controllerClientes.js
+++ controllers/controllerClientes.js
@@ -1,6 +1,3 @@
-const mysql = require("../mysql").pool
-
-
 exports.getClientes = (request, response, next) => {
     response.status(200).send({
         mensagem: "tudo certo nos clientes(get)"
@@ -9,24 +6,10 @@ exports.getClientes = (request, response, next) => {
 }
 
 exports.postClientes =  (request, response, next) => {
+    response.status(200).send({
+        mensagem: "tudo certo nos clientes(post)"
 
-    mysql.getConnection((error, conn)=>{
-        conn.query("INSERT INTO 3463659_usuario.clientes(enderecos_id, nome_cliente, telefone) values(?,?,?)",
-        [request.body.endereco_id, request.body.nome, request.body.telefone], (error, result, fields)=>{
-            conn.release()
-            if (error) {
-                return response.status(500).send({èrror: error})
-            }
-            response.status(200).send({
-                mensagem: "Cliente inserido com sucesso",
-                id_produto: result.insertId
-                
-            })
-        })
     })
-
-    
-
 }
 
 exports.patchClientes = (request, response, next) => {

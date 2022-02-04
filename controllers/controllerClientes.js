ee05032d9452c017a47c6b544cca6bc835283003
danilobserafim <danilob.serafim@gmail.com>
2022-02-04 11:06:00 -0300

    correção

--- controllers/controllerClientes.js
+++ controllers/controllerClientes.js
@@ -1,33 +1,32 @@
 const mysql = require("../mysql").pool
 
 exports.getClientes = (request, response, next) => {
-    response.status(200).send({
-        mensagem: "tudo certo nos clientes(get)"
-
-    })
+    response.status(200).send(
+        {
+            author: "Douglas",
+            content: "Lorem ipsum dolor sit amet consectetur.",
+            date: "13/01/2022"
+        },
+        {
+            author: "Mayara",
+            content: "Lorem ipsum dolor sit amet consectetur.",
+            date: "18/01/22"
+        },
+        {
+            author: "Marcos",
+            content: "Lorem ipsum dolor sit amet consectetur.",
+            date: "25/01/2022"
+        },
+        {
+            author: "Danilo",
+            content: "Lorem ipsum dolor sit amet consectetur.",
+            date: "03/02/2022"
+        },
+    )
 }
 
 exports.postClientes =  (request, response, next) => {
-    response.status(200).send({
-        author: "Douglas",
-        content: "Lorem ipsum dolor sit amet consectetur.",
-        date: "13/01/2022"
-    },
-    {
-        author: "Mayara",
-        content: "Lorem ipsum dolor sit amet consectetur.",
-        date: "18/01/22"
-    },
-    {
-        author: "Marcos",
-        content: "Lorem ipsum dolor sit amet consectetur.",
-        date: "25/01/2022"
-    },
-    {
-        author: "Danilo",
-        content: "Lorem ipsum dolor sit amet consectetur.",
-        date: "03/02/2022"
-    },
+    response.status(200).send(
     )
 }
 

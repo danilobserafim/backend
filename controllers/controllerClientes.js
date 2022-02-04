53553dc9417ff8a45fb2a5f23f72d20f8a4c350e
danilobserafim <danilob.serafim@gmail.com>
2022-02-04 11:01:49 -0300

    mokando sem alterar nomes

--- controllers/controllerClientes.js
+++ controllers/controllerClientes.js
@@ -9,9 +9,26 @@ exports.getClientes = (request, response, next) => {
 
 exports.postClientes =  (request, response, next) => {
     response.status(200).send({
-        mensagem: "tudo certo nos clientes(post)"
-
-    })
+        author: "Douglas",
+        content: "Lorem ipsum dolor sit amet consectetur.",
+        date: "13/01/2022"
+    },
+    {
+        author: "Mayara",
+        content: "Lorem ipsum dolor sit amet consectetur.",
+        date: "18/01/22"
+    },
+    {
+        author: "Marcos",
+        content: "Lorem ipsum dolor sit amet consectetur.",
+        date: "25/01/2022"
+    },
+    {
+        author: "Danilo",
+        content: "Lorem ipsum dolor sit amet consectetur.",
+        date: "03/02/2022"
+    },
+    )
 }
 
 exports.patchClientes = (request, response, next) => {
